d27d9b3cf168c4a264532954f629e63142bd07cc
rexllz <lzliurex@gmail.com>
2019-03-12 02:39:38 +0800

    add atomic

---
+++ src/main/java/com/hku/concurrency/example/atomic/AtomicExample6.java
@@ -0,0 +1,47 @@
+package com.hku.concurrency.example.atomic;
+
+import com.hku.concurrency.annoations.ThreadSafe;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+
+@Slf4j
+@ThreadSafe
+public class AtomicExample6 {
+
+    private static AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+    public static int clientTotal = 5000;
+    public static int threadTotal = 200;
+
+    public static void main(String[] args) throws Exception{
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i<clientTotal; i++){
+            executorService.execute(()->{
+                try {
+                    semaphore.acquire();
+                    test();
+                    semaphore.release();
+                }catch (Exception e){
+                    log.error("exception",e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("atomicBoolean:{}",atomicBoolean.get());
+    }
+    private static void test(){
+        if (atomicBoolean.compareAndSet(false,true))log.info("execute");
+    }
+
+}
